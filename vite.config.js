4cf259e8fc12d8d691f5b1f1c8b1fd32f3928fd2
Samrat Bhattarai <samratbhattarai6789@gmail.com>
2025-07-22 21:43:15 +0545

    Ready for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/",
+	base: "/New-Portfolio/", // 👈 use your repo name here
 	plugins: [react()],
 	test: {
 		globals: true,
-		environment: "jsdom", // important: use jsdom to provide DOM APIs
+		environment: "jsdom",
 	},
 });
